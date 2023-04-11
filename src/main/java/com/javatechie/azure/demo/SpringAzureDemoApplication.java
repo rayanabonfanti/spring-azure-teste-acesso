6b8fecb9bfed0b7aaa2bd8c259ef608d88484e45
rayana bonfanti <sr.rayana@gmail.com>
2023-04-11 12:48:25 -0300

    first commit

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -2,16 +2,24 @@ package com.javatechie.azure.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 public class SpringAzureDemoApplication {
 
-	@GetMapping("/message")
-	public String message(){
-		return "Congrats ! your application deployed successfully in Azure Platform. !";
+	@GetMapping("/chamada")
+	public String list() {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity("https://spring-azure-teste.azurewebsites.net/message", String.class);
+		String responseBody = response.getBody();
+		if(responseBody != null || responseBody != ""){
+			return "acerto";
+		}
+		return "erro";
 	}
 
 	public static void main(String[] args) {
